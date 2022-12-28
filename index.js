fd2e2e4d6fa04b8849ad51863c695e9fc086c4cb
amJayem <csejayem19@gmail.com>
2022-12-29 02:14:22 +0600

    getting user activities by post id

--- index.js
+++ index.js
@@ -18,44 +18,68 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
   const postCollection = client.db("social-people").collection("posts");
+  const activitiesCollection = client
+    .db("social-people")
+    .collection("activities");
 
   try {
     // storing post to db
     app.post("/post", async (req, res) => {
       const post = req.body;
       const result = await postCollection.insertOne(post);
-    //   console.log(post);
-      res.send(result)
+      //   console.log(post);
+      res.send(result);
     });
 
     // getting all post from db
-    app.get('/posts', async(req, res)=>{
-        const posts = await postCollection.find({}).toArray();
-        res.send(posts);
-        // console.log(posts);
+    app.get("/posts", async (req, res) => {
+      const posts = await postCollection.find({}).toArray();
+      res.send(posts);
+      // console.log(posts);
     });
 
     // getting single post by id
-    app.get('/post-details/:id',async(req, res) => {
-        const id = req.params.id;
-        const filter = { _id: ObjectId(id)};
-        const result = await postCollection.findOne(filter);
-        // console.log(id);
-        res.send(result);
+    app.get("/post-details/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await postCollection.findOne(filter);
+      // console.log(id);
+      res.send(result);
     });
 
     // updating like count
-    app.patch('/post-like/:id', async(req, res)=>{
-        const id = req.params.id;
-        const filter = { _id:ObjectId(id)};
-        const options = { upsert: true };
-        const like = req.body.value;
-        const updateLike = { $set: {like: like}}
-        const result = await postCollection.updateOne(filter, updateLike)
-        // console.log(like);
-        res.send(result)
-    })
+    app.patch("/post-like/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const like = req.body.value;
+      const updateLike = { $set: { like: like } };
+      const result = await postCollection.updateOne(filter, updateLike);
+      console.log(like);
+      res.send(result);
+    });
 
+    // save reaction to db
+    app.post("/my-reaction", async (req, res) => {
+      const relatedPostInfo = req.body;
+      const result = await activitiesCollection.insertOne(relatedPostInfo);
+      console.log(relatedPostInfo);
+      res.send(result);
+    });
+
+    // getting reaction from db for single user
+    app.get("/activities/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { postId: id };
+      const result = await activitiesCollection.findOne(filter);
+    //   console.log(result);
+      if (result) {
+        res.send(result);
+      }
+      else{
+        res.send({success: 'wait'})
+      }
+    });
   } finally {
   }
 };
